bf5cc3cf7c2860947060f9ffa72b410121dd715c
noinvis <lazizbek.sn@gmail.com>
2025-07-24 17:14:04 +0500

    Gatov

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,11 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import logo from "./img/Frame 168.png";
-import userIcon from "./img/user.png";
-import searchIcon from "./img/search.png";
-import heartIcon from "./img/heart.png";
-import cartIcon from "./img/cart.png";
+import { Link, NavLink } from "react-router-dom";
+import { IoCartOutline, IoClose, IoHeartOutline, IoHomeOutline, IoMenu, IoSearchOutline } from "react-icons/io5";
+import { FaRegUser } from "react-icons/fa";
 
 const Header = () => {
+  const [toggle, setToggle] = useState(false);
   return (
     <header className="max-w-[1440px] w-full mx-auto bg-white">
       <div className="w-full flex flex-col md:flex-row items-center justify-between px-4 md:px-8 py-4 gap-4 md:gap-0">
@@ -13,31 +13,66 @@ const Header = () => {
           <img src={logo} alt="Logo" className="h-8 w-auto" />
         </div>
 
-        <nav className="hidden md:flex space-x-6 text-sm lg:text-base font-medium text-gray-800">
-          <a href="#" className="hover:text-yellow-600">
-            Home
-          </a>
-          <a href="#" className="hover:text-yellow-600">
-            Shop
-          </a>
-          <a href="#" className="hover:text-yellow-600">
-            About
-          </a>
-          <a href="#" className="hover:text-yellow-600">
-            Contact
-          </a>
-        </nav>
+        <ul
+          className={`flex gap-4 e top-full left-0 max-sm:absolute max-sm:bg-white max-sm:w-full max-sm:flex-col max-sm:gap-2  max-sm:overflow-hidden max-sm:border-b max-sm:border-gray-200 duration-200 ${
+            toggle ? "max-sm:h-[110px]" : "max-sm:h-0"
+          }`}
+        >
+          <li className="max-sm:px-3 ">
+            <Link to={"/"}>Home</Link>
+          </li>
+          <li className="max-sm:px-3 ">
+            <Link to={"/about"}>About</Link>
+          </li>
+          <li className="max-sm:px-3 ">
+            <Link to={"/contact"}>Contact</Link>
+          </li>
+          <li className="max-sm:px-3 ">
+            <Link to={"/shop"}>Shop</Link>
+          </li>
+        </ul>
 
-        <div className="flex items-center space-x-4">
-          {[userIcon, searchIcon, heartIcon, cartIcon].map((icon, idx) => (
-            <img
-              key={idx}
-              src={icon}
-              className="w-5 h-5 md:w-6 md:h-6 cursor-pointer hover:scale-110 transition"
-              alt="icon"
-            />
-          ))}
-        </div>
+        <div className="flex gap-3 max-sm:fixed max-sm:bottom-0 max-sm:left-0 max-sm:w-full max-sm:justify-evenly max-sm:bg-white max-sm:py-4 max-sm:border-t max-sm:border-gray-300">
+                  <NavLink
+                    className={({ isActive }) =>
+                      `sm:hidden ${isActive ? "text-red-500" : ""}`
+                    }
+                    to={"/"}
+                  >
+                    <IoHomeOutline />
+                  </NavLink>
+                  <NavLink
+                    className={({ isActive }) => `${isActive ? "text-red-500" : ""}`}
+                    to={"/search"}
+                  >
+                    <IoSearchOutline />
+                  </NavLink>
+                  <NavLink
+                    className={({ isActive }) => `${isActive ? "text-red-500" : ""}`}
+                    to={"/wishlist"}
+                  >
+                    <IoHeartOutline />
+                  </NavLink>
+                  <NavLink
+                    className={({ isActive }) => `${isActive ? "text-red-500" : ""}`}
+                    to={"/cart"}
+                  >
+                    <IoCartOutline />
+                  </NavLink>
+                  <NavLink
+                    className={({ isActive }) => `${isActive ? "text-red-500" : ""}`}
+                    to={"/profile"}
+                  >
+                    <FaRegUser />
+                  </NavLink>
+                </div>
+        
+                <button
+                  className="text-2xl sm:hidden"
+                  onClick={() => setToggle((p) => !p)}
+                >
+                  {toggle ? <IoClose /> : <IoMenu />}
+                </button>
       </div>
     </header>
   );
