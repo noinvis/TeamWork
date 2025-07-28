e6f98f4b1fb449bff599d9766efab7ecff321019
Muhammadyusuf-Latifov <longhaochen200@gmail.com>
2025-07-28 05:31:10 +0500

    shop

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,8 @@ import DetailProduct from "./pages/detail-product/DetailProduct";
 import Wishlist from "./pages/wishlist.jsx/Wishlist";
 import Cart from "./pages/cart/Cart";
 import Contact from "./pages/contact/Contact";
+import Shop from "./pages/shop/Shop";
+import CategoryItem from "./pages/categoryItem/CategoryItem";
 
 const App = () => {
   return (
@@ -14,9 +16,12 @@ const App = () => {
         <Route path="/" element={<Layout />}>
           <Route index={true} element={<Home />} />
           <Route path="/product/:id" element={<DetailProduct />} />
-          <Route path="/wishlist" element={<Wishlist/>}/>
-          <Route path="/cart" element={<Cart/>}/>
-          <Route path="/contact" element={<Contact/>}/>
+          <Route path="/wishlist" element={<Wishlist />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/shop" element={<Shop />} />
+          <Route path="/products/category/:type" element={<CategoryItem />} />
+          <Route path="/contact" element={<Contact />} />
         </Route>
         <Route path="*" element={<NotFound />} />
       </Routes>
---
+++ src/components/category/Category.jsx
@@ -0,0 +1,35 @@
+import React, { useLayoutEffect } from "react";
+import { useFetch } from "../../hooks/useFetch";
+import "./Category.css";
+import { NavLink, useNavigate } from "react-router-dom";
+const Category = () => {
+  useLayoutEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
+  const { data } = useFetch("/products/category-list");
+
+  let offset = 0;
+
+  return (
+    <div className="bg-[#F9F1E7]">
+    
+        <div className="flex items-center   h-[100px] scrollbar-hide overflow-x-auto  border-[#c2c2c2] border-l-[1.5px] border-r-[1.5px] px-[3px]">
+          <ul className="flex items-center mx-auto  text-nowrap gap-[10px]  ">
+            {data?.map((item) => (
+              <li key={offset++}>
+                <NavLink
+                  to={`/products/category/${item}`}
+                  className="navlink duration-200 hover:bg-[#dac6af] px-[20px] py-[14px] rounded-[6px] h-[30px] text-[#555353] bg-[#f1e0cc]"
+                >
+                  <span> {item}</span>
+                </NavLink>
+              </li>
+            ))}
+          </ul>
+        </div>
+      
+    </div>
+  );
+};
+
+export default React.memo(Category);
