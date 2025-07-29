9de084409a8cc6302495f7e65da844b9ac23e3ab
noinvis <lazizbek.sn@gmail.com>
2025-07-29 12:16:28 +0500

    Finished

--- src/pages/blog/Blog.jsx
+++ src/pages/blog/Blog.jsx
@@ -19,6 +19,8 @@ import {
 
 function Blog() {
   return (
+    <>
+    <CartProps title={"Blog"} />
     <div className="max-w-6xl mx-auto p-4 grid grid-cols-1 md:grid-cols-3 gap-8">
       <div className="md:col-span-2 space-y-10 flex flex-col items-center">
         {[
@@ -128,6 +130,8 @@ function Blog() {
         </div>
       </div>
     </div>
+    <SectionProps />
+    </>
   );
 }
 
